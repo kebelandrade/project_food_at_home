8226829eafbed63e04380b9d635c778def712bbe
willysantos <willydasanez@gmail.com>
2019-03-05 12:48:36 -0600

    modelos

--- food_at_home/food_at_home/models.py
+++ food_at_home/food_at_home/models.py
@@ -1,33 +1,13 @@
 from django.db import models
 
 
-class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=40)
-    apellidos = models.CharField(max_length=40)
-    nombre_usuario = models.CharField(max_length=15, unique=True)
-    telefono = models.CharField(max_length=8)
-    email = models.EmailField()
-
-
-class DireccionUsuario(models.Model):
-    id = models.AutoField(primary_key=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
-    nombre = models.CharField(max_length=25)
-    direccion = models.CharField(max_length=150)
-
-
-class Transporte(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=20)
-
 
 class Restaurante(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     telefono = models.CharField(max_length=8)
     estado = models.BooleanField(default=True)
-    calificacion = models.IntegerField()
+    calificacion = models.CharField()
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
 
 
@@ -41,13 +21,15 @@ class DireccionRestaurante(models.Model):
     restaurante = models.ForeignKey(Restaurante, on_delete=models.PROTECT)
     ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT)
     ubicacion = models.CharField(max_length=150)
-
+    latitud = models.FloatField()
+    longitud = models.FloatField()
 
 class Empleado(models.Model):
     id = models.AutoField(primary_key=True)
     identidad = models.CharField(max_length=13, unique=True)
     nombre_completo = models.CharField(max_length=50)
-    usuario = models.CharField(max_length=15)
+    usuario = models.CharField(max_length=15, unique=True)
+    password = models.CharField()
     telefono = models.CharField(max_length=8)
 
 
@@ -58,6 +40,9 @@ class Pedido(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
     fecha_pedido = models.DateTimeField()
     direccion = models.ForeignKey(DireccionUsuario, on_delete=models.PROTECT)
+    calificacion = models.IntegerField()
+    precioPedido = models.DecimalField(max_digits=6,decimal_places=2)
+
 
 
 class Categoria(models.Model):
@@ -71,27 +56,26 @@ class Plato(models.Model):
     descripcion = models.CharField(max_length=250)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
+    img = models.CharField(max_length=100)
 
 
 class BodyPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
     plato = models.ForeignKey(Plato, on_delete=models.PROTECT)
     cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
 
-
-class CalificacionPedido(models.Model):
-    pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
-    calificacion = models.IntegerField()
-    observacion = models.CharField(max_length=250)
 
 
 class CalificacionRestaurante(models.Model):
-    id_pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
+    restaurante = models.ForeignKey(Restaurante, on_delete=models.PROTECT)
     calificacion = models.IntegerField()
-    Observacion = models.CharField(max_length=250)
+    observacion = models.CharField(max_length=250)
 
 
 class BitacoraEmpleado(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField()
     pedido = models.ForeignKey(Pedido, on_delete=models.PROTECT)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
