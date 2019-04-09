2c3c11fcd628f8b4de3fa8b5dee7ee1a1b955638
willysantos <willydasanez@gmail.com>
2019-04-08 22:47:55 -0600

    falto

--- food_at_home/food_at_home/views/index.py
+++ food_at_home/food_at_home/views/index.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from ..models import Usuario
+from django.http import HttpResponse, JsonResponse
+from django.core.exceptions import ValidationError
+from django.core import serializers
+from django.template.response import TemplateResponse
 
 def inicio(request):
     return render(request, 'Index.html')
 
+def login(request):
+    return render(request, 'login/login.html')
+
+def verificar(req):
+    errores = []
+    exito = True
+    pagina = 'login/login.html'
+    correo = req.POST.get('correo')
+    password = req.POST.get('contra')
+
+    try:
+        usuario = Usuario.objects.filter(['email','password'])
+        for usuario in user:
+            pass
+
+        # correo2 = usuario['email']
+        # if usuario.email == correo and usuario.password == password:
+        #     if tipo_usuario == 1:
+        #         exito = True
+        #         pagina = 'administrador/empleado.html'
+    except Exception as e:
+        errores = e.messages
+        exito = False
+        pagina = 'login/login.html'
+
+    if exito == True:
+        return JsonResponse({
+            'exito': exito,
+            'errores': errores,
+            'pagina': pagina,
+            'correo': correo,
+            'password': password,
+            # 'correo2': us
+            # 'password2': usuario.password
+            })
+    elif exito == False:
+        return JsonResponse({
+            'exito': exito,
+            'errores': errores,
+            'pagina': pagina
+            })
